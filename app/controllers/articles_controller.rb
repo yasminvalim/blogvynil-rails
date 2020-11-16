849b1956070539ee17be141d68f5b4fa7a19925e
YasminValim <yasmin.valimds@gmail.com>
2020-11-16 20:13:41 -0300

    Adding actions!

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,53 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+
+def new
+  @article = Article.new
+end
+
+def create
+  @article = Article.new(article_params)
+  if @article.save
+   flash[:notice] = "Article was successfully created"
+   redirect_to article_path(@article)
+  else
+   render 'new'
+  end
+end
+
+def show
+end
+
+def update
+  if @article.update(article_params)
+   flash[:notice] = "Article was updated"
+   redirect_to article_path(@article)
+  else
+   flash[:notice] = "Article was not updated"
+   render 'edit'
+  end
+end
+
+def edit
+end
+
+def index
+  @articles = Article.all
+end
+
+def destroy
+  @article.destroy
+  flash[:notice] = "Article was deleted"
+  redirect_to articles_path
+end
+
+private
+
+  def article_params
+   params.require(:article).permit(:title, :description)
+  end
+
+def set_article
+   @article = Article.find(params[:id])
+  end
 end
