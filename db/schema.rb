cf782031b279bf7456839fec4be37ec1c0ce2da1
YasminValim <yasmin.valimds@gmail.com>
2020-11-16 21:29:36 -0300

    User created and foreign key associations!

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_16_220230) do
+ActiveRecord::Schema.define(version: 2020_11_17_002620) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
