2038f1a4dcac71cc61d0d97be964f9d4d3a2f03b
YasminValim <yasmin.valimds@gmail.com>
2020-11-17 23:44:23 -0300

    User seeds!

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ActiveRecord::Base
   belongs_to :user
 
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
-  validates :description, presence: true, length: { minimum: 100, maximum: 1000 }
+  validates :description, presence: true, length: { minimum: 30, maximum: 1000 }
   validates :user_id, presence: true
 
   paginates_per 5
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: 'Ian Curtis', email: 'iancurtis@joydivision.com', password: '123456') 
+User.create(username: 'Robert Smith', email: 'robertsmith@thecure.com', password: '123456')
+User.create(username: 'Morrissey', email: 'morrissey@thesmiths.com', password: '123456')
+User.create(username: 'Ian Curtis', email: 'iancurtis@joydivision.com', password: '123456')
+User.create(username: 'Elizabeth Fraser', email: 'elizabeth@cocteautwins.com', password: '123456')
